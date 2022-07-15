9e3e899a7bceda1b255e188e7c2788ce253a377b
Deepak Goyal <dg887180@gmail.com>
2022-07-15 14:00:15 +0530

    deploy

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from "@testing-library/react";
-import App from "./App";
-import ReactDOM from "react-dom";
-
-test("renders learn react link", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
